adfca2892742b94c6fb44af2c503736a69733822
Andresa Lídia <86450495+andresalidia@users.noreply.github.com>
2024-07-31 14:51:04 -0400

    Add files via upload

---
+++ BoderLayoutDemo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package borderlayoutframe;
+import javax.swing. JFrame;
+/**
+ *
+ * @author Aluno
+ */
+public class BoderLayoutDemo {
+   public static void main( String[] args ){
+    BorderLayoutframe borderLayoutFrame = new BorderLayoutframe();
+    borderLayoutFrame. setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+    borderLayoutFrame. setSize( 300,200 ); // configura o tamanho do frame
+    borderLayoutFrame. setVisible( true ); // exibe o frame
+    } // fim de main 
+} // fim da classe BorderLayoutDemo
